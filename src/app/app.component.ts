a94d280ad914b326face5af5486f53332615f239
Rohan Nasina <rohannasina@gmail.com>
2018-06-13 14:17:33 -0400

    DataTable Angular6
    Client side display of data from a text file.
    #Logic remains the same for any other formats that you would be working with
    
    Steps to start the app:
    1. Download the repository
    2. run 'npm install'
    3. run 'npm start' or 'ng serve'
    
    Open up you localhost:4200

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  file:any;
+  data =  [
+    {Age: 19, Name: 'John', City: 'Bethesda', Email: 'john@gmail.com'},
+    {Age: 18, Name: 'John', City: 'Bethesda', Email: 'john@gmail.com'},
+    {Age: 17, Name: 'John', City: 'Bethesda', Email: 'john@gmail.com'},
+    {Age: 17, Name: 'John', City: 'Bethesda', Email: 'john@gmail.com'},
+    {Age: 19, Name: 'John', City: 'Doe', Email: 'john@gmail.com'},
+    {Age: 19, Name: 'John', City: 'Doe', Email: 'john@gmail.com'},
+    {Age: 19, Name: 'John', City: 'Bethesda', Email: 'john@gmail.com'},
+    {Age: 18, Name: 'John', City: 'Bethesda', Email: 'john@gmail.com'},
+    {Age: 17, Name: 'John', City: 'Bethesda', Email: 'john@gmail.com'},
+    {Age: 17, Name: 'John', City: 'Bethesda', Email: 'john@gmail.com'},
+    {Age: 19, Name: 'John', City: 'Doe', Email: 'john@gmail.com'},
+    {Age: 19, Name: 'John', City: 'Doe', Email: 'john@gmail.com'}
+  ];
+  fileChanged(e) {
+    let input = event.target;
+    for (var index = 0; index < input.files.length; index++) {
+        let reader = new FileReader();
+        reader.onload = () => {
+            this.data = [];
+            for(let s of reader.result.split('\n')) {
+              let a = s.split(' ');              
+              this.data.push({Age: a.shift(), Name: a.shift()+' '+a.shift(), City: a.shift(), Email: a.pop()});
+            }
+        }
+        reader.readAsText(input.files[index]);
+    };
+  }
 }
+
+export interface Element {
+  Age: number;
+  Name: string;
+  City: string;
+  Email: string;
+}
+
+const data: Element[] = [
+  {Age: 19, Name: 'John', City: 'Bethesda', Email: 'john@gmail.com'},
+  {Age: 18, Name: 'John', City: 'Bethesda', Email: 'john@gmail.com'},
+  {Age: 17, Name: 'John', City: 'Bethesda', Email: 'john@gmail.com'},
+  {Age: 17, Name: 'John', City: 'Bethesda', Email: 'john@gmail.com'},
+  {Age: 19, Name: 'John', City: 'Doe', Email: 'john@gmail.com'},
+  {Age: 19, Name: 'John', City: 'Doe', Email: 'john@gmail.com'},
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule  } from '@angular/common';
 import { NgModule } from '@angular/core';
-
+import {MatInputModule, MatTableModule, MatToolbarModule } from '@angular/material';
+import {DataTableModule} from "angular-6-datatable";
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [CommonModule, BrowserModule, DataTableModule, MatToolbarModule, MatInputModule, MatTableModule],
+  exports: [CommonModule, MatToolbarModule, MatInputModule, MatTableModule],
   providers: [],
   bootstrap: [AppComponent]
 })
